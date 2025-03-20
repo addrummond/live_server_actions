97e650c7de7c4ff15e9dd510753c9cdc222112e5
Alex Drummond <alex.drummond@multiverse.io>
2025-03-20 13:14:28 +0000

    remove old tailwind styling

--- examples/quotes/assets/js/quotes.tsx
+++ examples/quotes/assets/js/quotes.tsx
@@ -20,7 +20,6 @@ export const RandomQuoteChooser = () => {
       <p>
         <button
           type="button"
-          className="py-2 px-4 bg-zinc-900 text-white font-semibold rounded-lg shadow-md focus:outline-none focus:ring-2 focus:ring-green-400 focus:ring-opacity-75"
           onClick={() => {
             startTransition(() => {
               getQuoteAction();
