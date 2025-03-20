607a7c5a6e615b40fd0ce0b510da85a5f79df564
Alex Drummond <alex.drummond@multiverse.io>
2025-03-20 15:05:56 +0000

    class -> className

--- examples/quotes/assets/js/quotes.tsx
+++ examples/quotes/assets/js/quotes.tsx
@@ -10,7 +10,7 @@ export const RandomQuoteChooser = () => {
   );
   
   return <div>
-    <div class="quote-chooser">
+    <div className="quote-chooser">
       <p><label htmlFor="fruit">🍎🍍🍐&nbsp;&nbsp;Choose a fruit&nbsp;&nbsp;🍐🍍🍎</label></p>
       <select id="fruit" name="fruit" onChange={e => setFruit(e.target.value)} value={fruit}>
         <option value="Apple">Apple</option>
@@ -30,12 +30,12 @@ export const RandomQuoteChooser = () => {
         </button>
       </p>
     </div>
-    {quoteIsLoading ? <div class="lds-ring"><div></div></div> : <Quote quote={quote} />}
+    {quoteIsLoading ? <div className="lds-ring"><div></div></div> : <Quote quote={quote} />}
   </div>
 };
 
 function Quote({ quote }) {
-  return quote && <div class="quote">
+  return quote && <div className="quote">
     <p><i>{quote.quote}</i></p>
     <p>– {quote.author} ({quote.year})</p>
   </div>
