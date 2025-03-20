785be06be7a183559ef1b579f34d09f98347cfa0
Alex Drummond <alex.drummond@multiverse.io>
2025-03-20 10:17:40 +0000

    less strict LiveView version dependency

--- mix.exs
+++ mix.exs
@@ -23,7 +23,7 @@ defmodule LiveServerActions.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"}
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-      {:phoenix_live_view, "1.0.5"}
+      {:phoenix_live_view, "~> 1.0"}
     ]
   end
 end
